10449116533846a9210f1e52b6371626190865f8
r-ichinose <reoyinolai719@gmail.com>
2024-12-03 17:32:40 +0900

    トップページから新規登録の画面移動までできた

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
-        validates :nickname, presence: true
-        validates :first_name, presence: true
-        validates :last_name, presence: true
-        validates :first_name_kana, presence: true
-        validates :last_name_kana, presence: true
-        validates :birth_date, presence: true
+          validates :nickname, presence: true
+          validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must contain @ and be valid" }
+          validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include both letters and numbers" }, confirmation: true
+          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "must be full-width characters" }
+          validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "must be full-width characters" }
+          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "must be full-width katakana characters" }
+          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "must be full-width katakana characters" }
+          validates :birth_date, presence: true
 end
