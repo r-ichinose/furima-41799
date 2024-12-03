b4123fce410ea0bde86adef9a3c314c036ceac33
r-ichinose <reoyinolai719@gmail.com>
2024-12-04 01:30:46 +0900

    単体テスト実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               { 'テストユーザー' }
+    email                  { 'test@example.com' }
+    password               { 'password1' }
+    password_confirmation  { 'password1' }
+    first_name             { '太郎' }
+    last_name              { '山田' }
+    first_name_kana        { 'タロウ' }
+    last_name_kana         { 'ヤマダ' }
+    birth_date             { '1990-01-01' }
+  end
+end
