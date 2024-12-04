ffe50a260d0807bb153c6bf11f2b95399a7ff64e
r-ichinose <reoyinolai719@gmail.com>
2024-12-05 01:52:55 +0900

    render bundleが実装されていないので再検証

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,4 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rake db:migrate
