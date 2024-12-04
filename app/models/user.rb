334c19972aa80bffeb68e04dde667e7b113cc38f
r-ichinose <reoyinolai719@gmail.com>
2024-12-04 16:39:18 +0900

    emailのモデルにあるバリデーションを消した

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
           validates :nickname, presence: true
-          validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must contain @ and be valid" }
           validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include both letters and numbers" }, confirmation: true
           validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "must be full-width characters" }
           validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "must be full-width characters" }
