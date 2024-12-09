331b45daf78bb1ca9136acbc19cf324b27d3c867
r-ichinose <reoyinolai719@gmail.com>
2024-12-09 18:38:33 +0900

    モデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,8 +16,8 @@ class Item < ApplicationRecord
   validates :price, presence: true,numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true, numericality: { only_integer: true, other_than: 1,  message: 'must be greater than 1'}
 
-  def sold?
-    self.sold
+  def sold_out?
+    sold == true
   end
 
 
