b692291f026b94b1ca7ceb8ee07c62d405fbf062
r-ichinose <reoyinolai719@gmail.com>
2024-12-10 18:20:11 +0900

    バリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,6 @@ class Order < ApplicationRecord
 
   validates :user_id, presence: true
   validates :item_id, presence: true
+
+  accepts_nested_attributes_for :shipping_address
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,9 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
+
+  validates :postal_code, presence: true, 
+                          format: { with: /\A\d{3}-\d{4}\z/, message: "は正しい形式で入力してください" }
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :phone_number, presence: true
 end
