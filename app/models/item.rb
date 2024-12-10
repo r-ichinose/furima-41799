f0a773099a3dc34a04bcd787b9d4f7b9ad58c6bc
r-ichinose <reoyinolai719@gmail.com>
2024-12-10 18:01:47 +0900

    マイグレーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+  has_many :orders
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
