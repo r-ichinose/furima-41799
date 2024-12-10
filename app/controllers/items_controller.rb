fcb5cca7cf7491f65512c8e34a41d575d099e802
r-ichinose <reoyinolai719@gmail.com>
2024-12-10 15:22:19 +0900

    destroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
-  before_action :check_owner, only: [:edit, :update]
+  before_action :check_owner, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
