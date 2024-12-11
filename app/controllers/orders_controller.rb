327c78afbc3c0363f0d6243c78796336e2c7c683
r-ichinose <reoyinolai719@gmail.com>
2024-12-11 10:38:09 +0900

    フォームの情報を保存できる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @order = OrderForm.new
   end
+
+  def create
+    @order = OrderForm.new(order_params) # フォームオブジェクトにパラメータを渡す
+    if @order.save
+      redirect_to root_path, notice: '購入が完了しました'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_form).permit(:card_number, :expiration_date, :cvc).merge(user_id: current_user.id, item_id: @item.id)
+  end
+end
