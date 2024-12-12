2351a9381c54a17ee2102db455a3147a313fb610
r-ichinose <reoyinolai719@gmail.com>
2024-12-12 17:28:51 +0900

    バリデーションの削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,5 @@ class Order < ApplicationRecord
   belongs_to :item
   has_one :shipping_address
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-
   accepts_nested_attributes_for :shipping_address
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,8 +1,3 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
-
-  validates :post_code, presence: true, 
-                          format: { with: /\A\d{3}-\d{4}\z/, message: "は正しい形式で入力してください" }
-  validates :address, presence: true
-  validates :phone_number, presence: true
 end
