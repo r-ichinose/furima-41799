11ce15a7be55153119f715c65df2bc9ac304399e
r-ichinose <reoyinolai719@gmail.com>
2024-12-12 19:41:48 +0900

    クレジット決済の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,13 +7,15 @@ class OrdersController < ApplicationController
 
   def create
     @order = OrderForm.new(order_params)
-    if @order.save
-      redirect_to root_path, notice: '購入が完了しました'
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
     else
-      render :index
+      render 'index', status: :unprocessable_entity
     end
   end
-
+  
   private
 
   def set_item
@@ -21,6 +23,15 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_6fa68a78e45f8654bb028b97"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
