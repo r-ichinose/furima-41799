8cbdb40ef4505b28e0ced3e6a14dce99f9a30dde
r-ichinose <reoyinolai719@gmail.com>
2024-12-14 03:26:27 +0900

    テストコードの書き方修正また正常系を増やした

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -15,75 +15,85 @@ RSpec.describe OrderForm, type: :model do
     )
   end
 
-  describe 'OrderForm validations' do
-    context 'valid details' do
-      it 'is valid with all details' do
+  describe 'OrderFormのバリデーション' do
+    context '正常系' do
+      it '全ての情報が正しく入力されていれば有効である' do
+        expect(@order_form).to be_valid
+      end
+
+      it 'buildingが空でも有効である' do
+        @order_form.building = ''
+        expect(@order_form).to be_valid
+      end
+
+      it 'phone_numberが10桁でも有効である' do
+        @order_form.phone_number = '0901234567'
         expect(@order_form).to be_valid
       end
     end
 
-    context 'invalid details' do
-      it 'is invalid without a user_id' do
+    context '異常系' do
+      it 'user_idが空だと無効である' do
         @order_form.user_id = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("User can't be blank")
       end
 
-      it 'is invalid without an item_id' do
+      it 'item_idが空だと無効である' do
         @order_form.item_id = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Item can't be blank")
       end
 
-      it 'is invalid without a post_code' do
+      it 'post_codeが空だと無効である' do
         @order_form.post_code = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Post code can't be blank")
       end
 
-      it 'is invalid without a prefecture_id' do
+      it 'prefecture_idが空だと無効である' do
         @order_form.prefecture_id = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it 'is invalid if prefecture_id is 0' do
+      it 'prefecture_idが0だと無効である' do
         @order_form.prefecture_id = 0
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it 'is invalid with incorrectly formatted post_code' do
+      it 'post_codeが不正な形式だと無効である' do
         @order_form.post_code = '1234567'
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
       end
 
-      it 'is invalid without a city' do
+      it 'cityが空だと無効である' do
         @order_form.city = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("City can't be blank")
       end
 
-      it 'is invalid without an address' do
+      it 'addressが空だと無効である' do
         @order_form.address = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Address can't be blank")
       end
 
-      it 'is invalid without a phone_number' do
+      it 'phone_numberが空だと無効である' do
         @order_form.phone_number = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Phone number can't be blank")
       end
 
-      it 'is invalid with incorrectly formatted phone_number' do
+      it 'phone_numberが不正な形式だと無効である' do
         @order_form.phone_number = '123456789'
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
 
-      it 'is invalid without a token' do
+      it 'tokenが空だと無効である' do
         @order_form.token = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Token can't be blank")
